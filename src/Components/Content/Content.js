3d28f773c8138e27ca336247571adaa80caae608
nsandobal <nsandobalmora@santandertecnologia.com.ar>
2022-01-04 14:14:47 -0300

    Default no flex empleados

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -82,7 +82,7 @@ const Content = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quaerat culpa, quam ipsa sit illum cupiditate dolorem. Temporibus dolorem cumque possimus eum sapiente, debitis laudantium earum nostrum! Enim, labore quisquam!</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum excepturi culpa perspiciatis id, sapiente soluta eligendi illo reiciendis omnis sunt iure debitis repellendus consequuntur rerum, ipsa minima incidunt. Commodi, perferendis!</p>
             <h2 className='title'>{ title }</h2>
-            <div className='empleados empleados-flex'>
+            <div className='empleados'>
                 <div className='box-container'>
                     <h2>Empleados destacados</h2>
                     {
